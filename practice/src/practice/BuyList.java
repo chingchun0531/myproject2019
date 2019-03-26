d4a9613b0fe0a46717f1acc0d8e94bbcb01566af
user <user@es301n15>
2019-03-26 11:39:21 +0800

    待購清單

---
+++ practice/src/practice/BuyList.java
@@ -0,0 +1,30 @@
+package practice;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+
+
+public class BuyList {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		List<Item>items=new ArrayList<>();
+		Scanner scanner = new Scanner(System.in);
+		String name = "";
+		while(!name.equals("q")) {
+			System.out.print("Enter item's name :");
+			name = scanner.nextLine();
+			System.out.print("Enter item's quantity :");
+			int qty = Integer.parseInt(scanner.nextLine());
+			items.add(new Item(name,qty));
+		}
+		for(Item item : items ) {
+			System.out.print(item.name + "\t" + item.qty);
+		}
+}
+	
+
+	}
---
+++ practice/src/practice/Item.java
@@ -0,0 +1,12 @@
+package practice;
+
+public class Item {
+
+	public int qty;
+	public String name;
+
+	public Item(String name, int qty) {
+		// TODO Auto-generated constructor stub
+	}
+
+}
